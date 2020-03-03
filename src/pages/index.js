8a0d27463b854483140f2e800a6034fb3c7a91d3
segun-ade <adebanjosegun589@gmail.com>
2020-03-03 06:07:36 +0100

    add project section and contact form:rocket:

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,11 @@ const IndexPage = () => (
     <div className="container-fluid p-0">
       <section
         className="resume-section"
-        id="about"
+        id="top"
       >
         <div className="w-100 flex" data-aos="fade-up"
           data-aos-duration="3000">
-          <div className="mb-0">
+          <article className="mb-0">
             <span className="salute offwhite">Hi, I'm</span>
             <h1 className="name heading">{config.firstName} {config.lastName},</h1>
             <h1 className="role heading">I {config.role}</h1>
@@ -23,12 +23,12 @@ const IndexPage = () => (
               {config.about}
             </p>
             <button className="cta-btn">{config.cta}</button>
-          </div>
+          </article>
           <div className="about">
             <a className="about-link" href="https://github.com/Segun-Ade">
-              <div className="avatar">
-                <img src={config.avatar} />
-              </div>
+              <figure className="avatar">
+                <img src={config.avatar} alt="avatar" />
+              </figure>
             </a>
           </div>
           {/* <div className="social-icons">
@@ -46,16 +46,16 @@ const IndexPage = () => (
 
       <section
         className="resume-section white-bg"
-        id="experience"
+        id="about"
       >
-        <div className="w-100 flex">
-          <div className="mb-0">
+        <div className="w-100 flex bg-white">
+          <article className="mb-0">
             <div>
               <h1 className="about-h blue-2 heading">About me</h1>
               <p className="lead mb-5 black">{config.aboutMe}</p>
             </div>
-          </div>
-          <div className="wd-100">
+          </article>
+          <article className="wd-100">
             <div>
               <h3 className="blue-2">Core Technologies</h3>
               <ul className="list-n">
@@ -76,182 +76,58 @@ const IndexPage = () => (
                 })}
               </ul>
             </div>
-          </div>
+          </article>
         </div>
       </section>
 
-
-      {/*<section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="education"
-      >
-        <div className="w-100">
-          <h2 className="mb-5">Education</h2>
-
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-            <div className="resume-content">
-              <h3 className="mb-0">University of Colorado Boulder</h3>
-              <div className="subheading mb-3">Bachelor of Science</div>
-              <div>Computer Science - Web Development Track</div>
-              <p>GPA: 3.23</p>
-            </div>
-            <div className="resume-date text-md-right">
-              <span className="text-primary">August 2006 - May 2010</span>
-            </div>
-          </div>
-
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
-            <div className="resume-content">
-              <h3 className="mb-0">James Buchanan High School</h3>
-              <div className="subheading mb-3">Technology Magnet Program</div>
-              <p>GPA: 3.56</p>
-            </div>
-            <div className="resume-date text-md-right">
-              <span className="text-primary">August 2002 - May 2006</span>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <hr className="m-0" />
 
       <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="skills"
+        className="resume-section"
+        id="projects"
       >
         <div className="w-100">
-          <h2 className="mb-5">Skills</h2>
+          <h2 className="projects heading">Recent Projects</h2>
 
-          <div className="subheading mb-3">
-            Programming Languages &amp; Tools
+          <div className="flex-card">
+            {config.projects.map(project => {
+              const { github, index, name, url, } = project;
+              return (
+                <a key={name} href={url}>
+                  <div className={'card' + ' ' + 'numb_' + index}></div>
+                </a>
+              )
+            })}
           </div>
-          <ul className="list-inline dev-icons">
-            <li className="list-inline-item">
-              <i className="fab fa-html5"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-css3-alt"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-js-square"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-angular"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-react"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-node-js"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-sass"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-less"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-wordpress"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-gulp"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-grunt"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-npm"></i>
-            </li>
-          </ul>
-
-          <div className="subheading mb-3">Workflow</div>
-          <ul className="fa-ul mb-0">
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Mobile-First, Responsive Design
-            </li>
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Cross Browser Testing &amp; Debugging
-            </li>
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Cross Functional Teams
-            </li>
-            <li>
-              <i className="fa-li fa fa-check"></i>
-              Agile Development &amp; Scrum
-            </li>
-          </ul>
         </div>
       </section>
 
-      <hr className="m-0" />
-
       <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="interests"
-      >
-        <div className="w-100">
-          <h2 className="mb-5">Interests</h2>
-          <p>
-            Apart from being a web developer, I enjoy most of my time being
-            outdoors. In the winter, I am an avid skier and novice ice climber.
-            During the warmer months here in Colorado, I enjoy mountain biking,
-            free climbing, and kayaking.
-          </p>
-          <p className="mb-0">
-            When forced indoors, I follow a number of sci-fi and fantasy genre
-            movies and television shows, I am an aspiring chef, and I spend a
-            large amount of my free time exploring the latest technology
-            advancements in the front-end web development world.
-          </p>
-        </div>
-      </section>
-
-      <hr className="m-0" />
-
-      <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        className="resume-section white-bg"
         id="awards"
       >
-        <div className="w-100">
-          <h2 className="mb-5">Awards &amp; Certifications</h2>
-          <ul className="fa-ul mb-0">
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>
-              Google Analytics Certified Developer
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>
-              Mobile Web Specialist - Google Certification
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition
-              2009
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - University of Colorado Boulder - Adobe Creative Jam 2008
-              (UI Design Category)
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>2<sup>nd</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition
-              2008
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - James Buchanan High School - Hackathon 2006
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>3<sup>rd</sup>
-              Place - James Buchanan High School - Hackathon 2005
-            </li>
-          </ul>
+        <div className="w-100 bg-white">
+          <div>
+            <article>
+              <h2 className="contact blue-2 heading">Get in touch</h2>
+              <p className="lead mb-5 black">{config.sayHi}</p>
+            </article>
+            <form
+              action="https://formspree.io/mgeylaje"
+              method="POST"
+            >
+              {config.formStrings.map(str => {
+                const { name, placeHolder, type } = str;
+                return (
+                  <>
+                    <input className="a1" key={name} type={type} name={name} placeholder={placeHolder} />
+                  </>
+                )
+              })}
+              <button className="">Send Message</button>
+            </form>
+          </div>
         </div>
-      </section> */}
+      </section>
     </div>
   </Layout>
 );
